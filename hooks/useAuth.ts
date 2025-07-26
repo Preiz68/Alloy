eb6e5392e0a43883ebb93c58d4955a8140a57d51
Akoji Praise <akojiomede75@gmail.com>
2025-07-26 18:58:33 +0100

    Created custom hook to manage authentication state

---
+++ hooks/useAuth.ts
@@ -0,0 +1,21 @@
+import { useState,useEffect } from "react"
+import { onAuthStateChanged, User } from "firebase/auth"
+import {auth} from "@/lib/firebase"
+
+export const useAuth = () => {
+   const [user,setUser] = useState<User | null>(null)
+   const [loading,setLoading] = useState<boolean>(true)
+
+   useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,(firebaseUser) => {
+        setUser(firebaseUser)
+        setLoading(false)
+    })
+
+    return () => unsubscribe()
+
+   },[])
+
+   return {user,loading}
+}
+export default useAuth

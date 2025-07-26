bb9db21be32d2814af9a2dad3b5abfa644d3bdf7
Akoji Praise <akojiomede75@gmail.com>
2025-07-26 19:00:29 +0100

    Made use of zod for strong form validation

---
+++ schemas/signupSchema.ts
@@ -0,0 +1,19 @@
+import {z} from "zod"
+
+
+export const signUpSchema = z.object({
+    email:z
+    .email("Invalid email address")
+    .refine((val) => val.endsWith("@gmail.com") || val.endsWith("@yahoo.com"),{
+        message:"Only Gmail and yahoo are allowed"
+    }),
+    password:z
+    .string()
+    .min(8, "Password must be 8 characters")
+    .regex(/[A-Z]/,"Password must contain at least one uppercase letter")
+    .regex(/[a-z]/,"Password must contain at least one lowercase letter")
+    .regex(/[0-9]/,"Password must contain at least one number")
+    .regex(/[@$!%*?&]/,"Password must contain at least one special character(@$!%*?&)")
+})
+
+export type signUpFormData = z.infer<typeof signUpSchema>

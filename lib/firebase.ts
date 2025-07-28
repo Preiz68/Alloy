519204986ca08250049c5c7fafd3d567435ee3ad
Akoji Praise <akojiomede75@gmail.com>
2025-07-28 08:48:02 +0100

    I added a line of code that checks if an app has been initialized already

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp,getApps,getApp } from "firebase/app";
 import { getAuth,GithubAuthProvider,GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig):getApp()
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 export const githubProvider = new GithubAuthProvider()

5fc84e98c8c63e0a74e5a873a36c076eb26b54ce
Akoji Praise <akojiomede75@gmail.com>
2025-09-10 03:30:50 +0100

    removed export

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import {Space_Grotesk} from "next/font/google";
 
 
 
-export const spaceGrotesk = Space_Grotesk({
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
 });
